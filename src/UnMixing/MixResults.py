1719427057e580f9ad16e22b06282e1ab9a790cf
acranej <ajc406@case.edu>
2024-04-30 14:01:30 -0400

    update results class

--- src/UnMixing/MixResults.py
+++ src/UnMixing/MixResults.py
@@ -1,3 +1,20 @@
-
-class MixResults(object): 
-    x = 1
+from dataclasses import dataclass 
+@dataclass(frozen = True)
+class MixResults:
+    w: float
+    mu: float
+    sigma: float 
+    
+    @property
+    def meanW(self) -> float:
+        return self.w.mean
+    
+    @property
+    def meanMu(self) -> float:
+        return self.mu.mean
+    
+    @property
+    def meanSigman(self) -> float:
+        return self.sigma.mean
+    
+    
