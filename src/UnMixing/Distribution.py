b0c4f5c5ebe97e87850015e6a35ab5fd06c2df9e
acranej <ajc406@case.edu>
2024-05-01 13:30:04 -0400

    planning out dists

--- src/UnMixing/Distribution.py
+++ src/UnMixing/Distribution.py
@@ -4,4 +4,15 @@ class Distribution:
     mu: float
     sigma: float
 
-    
+
+def TruncGaussian() -> Distribution:
+    lower = 6
+    upper = 8
+
+def Gaussian() -> Distribution:
+    mu = 1
+    sigma = 0
+
+def Exponential() -> Distribution:
+    mu = 1
+    sigma = 0
