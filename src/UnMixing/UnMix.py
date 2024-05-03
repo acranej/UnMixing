71e598a40dd8c6b3f8b9e31837e419819aff391a
acranej <ajc406@case.edu>
2024-05-03 17:30:49 -0400

    pkl error with pymc temp solution

--- src/UnMixing/UnMix.py
+++ src/UnMixing/UnMix.py
@@ -1,18 +1,19 @@
 import numpy as np
 import pymc as pm
+import multiprocessing 
 import sys
 sys.path.append('/Users/alexander_crane/Desktop/Research/UnMixing/')
 from src.UnMixing import MixResults, Distribution_make
+
+# multiprocessing.set_start_method('fork')
 
 def UnMix(dist1, dist2):
     print("UnMixing...")
     if dist1.type and dist2.type == "Truncated_Normal":
         with pm.Model() as model:
-            w = pm.Dirichlet('w', a=np.array([1, 1]))
+            w = pm.Dirichlet("w", a=np.array([1, 1]))
             mu1 = pm.Normal("mu1", mu=dist1.mu, sigma=dist1.sigma)
-            sig1 = pm.HalfNormal('sig1', sigma=dist1.sigma)
-            mu2 = pm.Normal("mu2", mu=dist2.mu, sigma=dist2.sigma)
-            sig2 = pm.HalfNormal('sig2', sigma=dist2.sigma)
+            sig1 = pm.HalfNormal("sig1", sigma=dist1.sigma)
 
             dist1_temp = pm.TruncatedNormal.dist(mu=mu1,
                                                  sigma=sig1,
@@ -28,14 +29,15 @@ def UnMix(dist1, dist2):
                               w=w,
                               comp_dists=[dist1_temp,dist2_temp],
                               observed=dist1.values)
-            model.debug()
+            #model.debug()
         with model:
-            idata=pm.sample(draws=1000, tune=1000, target_accept=0.95, chains = 1)
+            idata=pm.sample(draws=1000, tune=1000, target_accept=0.95, cores = 1)
 
         mu_temp = idata.posterior["mu1"].mean(("chain", "draw"))
         sig_temp = idata.posterior["sig1"].mean(("chain", "draw"))
         w_temp = idata.posterior["w"].mean(("chain", "draw"))
         print("Generating results...")
-        res = MixResults.MixResults(mu_temp, sig_temp, w_temp, dist1.values, dist1.mle, dist2.values, dist2.mle)
+        res = MixResults.MixResults(w = w_temp, mu = mu_temp, sigma= sig_temp, target_vals = dist1.values, 
+                                    target_mle = dist1.mle, inert_vals = dist2.values, inert_mle = dist2.mle)
 
     return res
