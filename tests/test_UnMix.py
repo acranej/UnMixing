4903a2aa8450a8d99b67edd3f6ba6d40f181f50e
acranej <ajc406@case.edu>
2024-05-04 14:51:03 -0400

    mixed distribution test

--- tests/test_UnMix.py
+++ tests/test_UnMix.py
@@ -8,10 +8,13 @@ rng = np.random.default_rng()
 
 class TestUnMix(unittest.TestCase):
     def test_1(self):
-        a = rng.normal(loc= 5, scale = 0.7, size = 100000)
-        b = rng.normal(loc= 5, scale = 0.7, size = 100000)
-        a_dist = Distribution_make.TruncNormal(a)
-        b_dist = Distribution_make.TruncNormal(b)
+        ### dist 2 must be the one that makes MLE for now
+        inert = rng.normal(loc= 5.8, scale = 0.8, size = 10000) 
+        inert_comp = rng.normal(loc= 5.9, scale = 1, size = 1000)
+        target_comp = rng.normal(loc= 6.3, scale = 0.9, size = 9000) 
+        target = np.concatenate((inert_comp, target_comp))
+        a_dist = Distribution_make.TruncNormal(target, target= True) 
+        b_dist = Distribution_make.TruncNormal(inert, target = False) 
         res = UnMix.UnMix(a_dist, b_dist)
         
         MixResults.plot_MixResults(res)
